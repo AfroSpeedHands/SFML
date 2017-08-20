09fb7051e048e0b306def2e7e098d87cccbe2042
rmoon3 <rmoon3@lsu.edu>
2017-08-19 20:03:20 -0500

    Add project files.

---
+++ Game.h
@@ -0,0 +1,14 @@
+class Game
+{
+	public:
+		Game();
+		void run();
+
+	private:
+		void processEvents();
+		void update();
+		void render();
+
+	private:
+		sf::RenderWindow 
+};
---
+++ Tutorial/Tutorial.cpp
@@ -0,0 +1,12 @@
+// Tutorial.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Game.h"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	Game game;
+	game.run();
+}
